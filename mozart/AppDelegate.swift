ad27b32ce9bcb901e006cd207c645435826f6eda
David Chan <davidkwokhochan@gmail.com>
2017-02-08 16:00:20 -0800

    Initial Commit

---
+++ mozart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mozart
+//
+//  Created by David Kwok Ho Chan on 2/8/17.
+//  Copyright © 2017 David Kwok Ho Chan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mozart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mozart
+//
+//  Created by David Kwok Ho Chan on 2/8/17.
+//  Copyright © 2017 David Kwok Ho Chan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
